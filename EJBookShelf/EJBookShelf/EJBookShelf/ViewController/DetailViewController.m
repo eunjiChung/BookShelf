2526eb3d56831582ee4fd206b01374b4d92cc78f
eunjiChung <twih@naver.com>
2019-10-25 13:31:29 +0900

    commit

--- EJBookShelf/EJBookShelf/EJBookShelf/ViewController/DetailViewController.m
+++ EJBookShelf/EJBookShelf/EJBookShelf/ViewController/DetailViewController.m
@@ -9,6 +9,9 @@
 #import "DetailViewController.h"
 #import "BookTableViewCell.h"
 #import "EJHTTPClient.h"
+#import "DetailBookDescTableViewCell.h"
+#import "DetailBookInfoTableViewCell.h"
+#import "DetailBookImageTableViewCell.h"
 
 @interface DetailViewController () <UITableViewDataSource, UITableViewDelegate>
 
@@ -31,7 +34,6 @@
     [[EJHTTPClient sharedInstance] requestDetailBookInfo:self.isbn13 success:^(id  _Nonnull result) {
         NSDictionary *dict = result;
         self.bookInfo = dict;
-        NSLog(@"Detail: %@", self.bookInfo);
         [self.tableview reloadData];
     } failure:^(NSError * _Nonnull error) {
         NSLog(@"Error: %@", error);
@@ -47,13 +49,26 @@
 
 #pragma mark  - TableView Data source
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    return 3;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    BookTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"BookTableViewCell" forIndexPath:indexPath];
+    switch (indexPath.row) {
+        case 0:
+            DetailBookImageTableViewCell *cell
+            DetailBookImageTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ResultInfoTableViewCell" forIndexPath:indexPath];
+            return cell;
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        default:
+            NSLog(@"No Cell");
+            break;
+    }
     
-    return cell;
+    return [[UITableViewCell alloc] init];
 }
 
 @end
