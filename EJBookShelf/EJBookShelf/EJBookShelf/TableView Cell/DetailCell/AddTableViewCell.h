90e27eff20d41772461bb9e776a39cb4ed9ee7f3
eunjiChung <twih@naver.com>
2019-10-26 19:22:01 +0900

    Try adding memo to detail view controller

---
+++ EJBookShelf/EJBookShelf/EJBookShelf/TableView Cell/DetailCell/AddTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  AddTableViewCell.h
+//  EJBookShelf
+//
+//  Created by CHUNGEUNJI on 26/10/2019.
+//  Copyright © 2019 eunji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ EJBookShelf/EJBookShelf/EJBookShelf/TableView Cell/DetailCell/MemoTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MemoTableViewCell.m
+//  EJBookShelf
+//
+//  Created by CHUNGEUNJI on 26/10/2019.
+//  Copyright © 2019 eunji. All rights reserved.
+//
+
+#import "MemoTableViewCell.h"
+
+@implementation MemoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
